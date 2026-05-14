f3e02f3a4795e44a0e54c13636b471e3ea042833
charlene tan <chartsj@outlook.com>
2026-05-15 00:41:07 +0800

    test

--- login_UI/src/components/LoginForm.vue
+++ login_UI/src/components/LoginForm.vue
@@ -7,7 +7,7 @@ const loading = ref(false)
 const errorMessage = ref('')
 
 const submit = () => {
-  errorMessage.value = ''
+  errorMessage.value = 'test'
 
   if (!email.value || !password.value) {
     errorMessage.value = 'Email and password are required d.'
